aa1ac3bdb0a746e89885d80ba59fce455e2355e8
Tsering Dhondup <tyddhondup88@gmail.com>
2023-08-24 22:42:45 -0400

    comments array was created

---
+++ scripts/index-page.js
@@ -0,0 +1,27 @@
+// COMMENT ARRAY
+let comments = [
+  {
+    name: "Connor Walton",
+    timestamp: "02/17/2021",
+    commentText: `This is art. 
+    This is inexplicable magic expressed in the purest way, 
+    everything that makes up this majestic work deserves reverence.
+     Let us appreciate this for what it is and what it contains. `,
+  },
+  {
+    name: "Emilie Beach",
+    timestamp: "01/09/2021",
+    commentText: `I feel blessed to have seen them in
+    person. What a show! They were just
+    perfection. If there was one day of my
+    life I could relive, this would be it. What
+    an incredible day.`,
+  },
+  {
+    name: "Miles Acosta",
+    timestamp: "12/20/2020",
+    commentText: `This is art. 
+    This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. 
+    Let us appreciate this for what it is and what it contains. `,
+  },
+];
