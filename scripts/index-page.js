45eaf10d0820d16393cf14b2dc2d2c3284a63311
Tsering Dhondup <tyddhondup88@gmail.com>
2023-08-27 18:33:05 -0400

    value for commentText key in comments object were restructure

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,26 +3,20 @@ let comments = [
   {
     name: "Connor Walton",
     timestamp: "02/17/2021",
-    commentText: `This is art. 
-    This is inexplicable magic expressed in the purest way, 
-    everything that makes up this majestic work deserves reverence.
-     Let us appreciate this for what it is and what it contains. `,
+    commentText:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains. ",
   },
   {
     name: "Emilie Beach",
     timestamp: "01/09/2021",
-    commentText: `I feel blessed to have seen them in
-    person. What a show! They were just
-    perfection. If there was one day of my
-    life I could relive, this would be it. What
-    an incredible day.`,
+    commentText:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
     name: "Miles Acosta",
     timestamp: "12/20/2020",
-    commentText: `This is art. 
-    This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. 
-    Let us appreciate this for what it is and what it contains. `,
+    commentText:
+      "This is art.This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains. ",
   },
 ];
 // DISPLAY COMMENTS - RENDERING INTO DOM
@@ -77,7 +71,6 @@ const createEl = (comments) => {
     textContainerEl.appendChild(commentTextEl); // APPENDING TO textContainerEl
 
     commentsContainerEl.appendChild(textContainerEl); // APPENDING TEXT-CONTAINER INTO commentContainerEl
-
     // INVOKING displayComment FUNCTION
     displayComment(commentsContainerEl);
   });
