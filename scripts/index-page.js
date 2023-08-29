29871ce0193089e774b2fefde34a658190f2e439
Tsering Dhondup <tyddhondup88@gmail.com>
2023-08-28 21:21:22 -0400

    value for commentText for name: Miles Acosta was changed

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -16,7 +16,7 @@ let comments = [
     name: "Miles Acosta",
     timestamp: "12/20/2020",
     commentText:
-      "This is art.This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains. ",
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
 // DISPLAY COMMENTS - RENDERING INTO DOM
