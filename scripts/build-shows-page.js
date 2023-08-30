8834b2d1ad0413a2ea1548ef2374f3be42158cef
Tsering Dhondup <tyddhondup88@gmail.com>
2023-08-30 00:20:13 -0400

    createShowEl()  and renderingFunction() were created

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,6 +1,6 @@
 // SHOWS-INFO ARRAY
 
-let showsInfo = [
+let showsDatas = [
   {
     date: "Mon Sept 06 2021",
     venue: "Ronald Lane",
@@ -33,7 +33,7 @@ let showsInfo = [
   },
 ];
 
-//  DOM RENDERING FUNCTION
+//  DOM-RENDERING FUNCTION()
 
 let renderingFunction = (headerEl, showsEl) => {
   let showsContainer = document.querySelector(".shows");
@@ -42,3 +42,85 @@ let renderingFunction = (headerEl, showsEl) => {
   // APPENDING SHOWS-TABLE
   showsContainer.appendChild(showsEl);
 };
+
+// CREATE-ELEMENTS FUNCTION()
+let createShowEl = (datas) => {
+  // SHOWS__TITLE - " to be passed as an argument for renderingFunction() "
+  let showsTitleEl = document.createElement("h1");
+  showsTitleEl.classList.add("shows__title");
+  showsTitleEl.innerText = "Shows";
+
+  // LOOPING THROUGH THE SHOWS-DATAS ARRAY
+  showsDatas.forEach((element) => {
+    /*-------------------------------------*/
+
+    // SHOW-TABLE-CONTAINER - " to be passed as an argument for renderingFunction() "
+    let showsTableContainerEl = document.createElement("div");
+    showsTableContainerEl.classList.add("shows__table-container");
+
+    // SECTION -1 : SHOWS-TABLE-HEADERS-CONTAINER
+
+    let headerEl = document.createElement("div");
+    headerEl.classList.add("shows-table-headers-container");
+    showsTableContainerEl.appendChild(headerEl); // APPENDING TO SHOW-TABLE-CONTAINER
+
+    // SHOWS-TABLE-HEADER__NAME - DATE
+    let headersDateEl = document.createElement("h3");
+    headersDateEl.classList.add("shows-table-headers-container__name");
+    headersDateEl.innerText = "Date";
+    headerEl.appendChild(headersDateEl); // APPENDING TO SHOW-HEADERS
+
+    // SHOWS-TABLE-HEADER__NAME - VENUE
+    let headersVenueEl = document.createElement("h3");
+    headersVenueEl.classList.add("shows-table-headers-container__name");
+    headersDateEl.innerText = "Venue";
+    headerEl.appendChild(headersVenueEl); // APPENDING TO SHOW-HEADERS
+
+    // SHOWS-TABLE-HEADER__NAME - LOCATION
+    let headersLocationEl = document.createElement("h3");
+    headersLocationEl.classList.add("shows-table-headers-container__name");
+    headersDateEl.innerText = "Location";
+    headerEl.appendChild(headersLocationEl); // APPENDING TO SHOW-HEADERS
+
+    // SECTION -2 : SHOWS-TABLE-DATA-CONTAINER
+
+    let tableDataContainerEl = document.createElement("div");
+    tableDataContainerEl.classList.add("shows-table-data-container");
+    showsTableContainerEl.appendChild(tableDataContainerEl); // APPENDING TO SHOW-TABLE-CONTAINER
+
+    // SHOW-TABLE-DATA-CONTENT
+    let tableDataContentEl = document.createElement("div");
+    tableDataContentEl.classList.add("shows-table-data-container__content");
+    tableDataContainerEl.appendChild(tableDataContentEl); // APPENDING TO SHOW-TABLE-DATA-CONTAINER
+
+    // SHOWS-TABLE-DATAS__DATE
+    let dateEl = document.createElement("p");
+    dateEl.classList.add("shows-table-data__content--name");
+    tableDataContentEl.appendChild(dateEl); // APPENDING TO SHOW-TABLE-DATA-CONTENT
+
+    // SHOWS-TABLE-DATAS__VENUE
+    let venueEl = document.createElement("p");
+    venueEl.classList.add("shows-table-data__content--venue");
+    tableDataContentEl.appendChild(venueEl); // APPENDING TO SHOW-TABLE-DATA-CONTENT
+
+    // SHOWS-TABLE-DATAS__LOCATION
+    let locationEl = document.createElement("p");
+    locationEl.classList.add("shows-table-data__content--location");
+    tableDataContentEl.appendChild(locationEl); // APPENDING TO SHOW-TABLE-DATA-CONTENT
+
+    // SHOWS-TABLE-DATAS__BUTTON
+    let buttonEl = document.createElement("button");
+    locationEl.classList.add("shows-table-data__content--btn");
+    buttonEl.innerText = "Buy Ticket";
+    tableDataContentEl.appendChild(buttonEl); // APPENDING TO SHOW-TABLE-DATA-CONTENT
+    dateEl.innerText = element.date;
+    venueEl.innerText = element.venue;
+    locationEl.innerText = element.location;
+
+    // INVOKING REDERING-FUNCTION()
+    renderingFunction(showsTitleEl, showsTableContainerEl);
+  });
+};
+
+// INVOKING CREATE-ELEMENTS-FUNCTION
+createShowEl(showsDatas);
